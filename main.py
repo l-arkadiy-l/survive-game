ac3c16717a018f8206eddde9c0a987602d7377c8
|-arkadiy-| <arkadiy.kleymenov11@yandex.ru>
2020-12-10 12:34:56 +0300

    create prototipe enemy

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from random import randrange
 import pygame
 from pygame.math import Vector2
+import math
+
+
+class Hard_Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos, *groups):
+        super(Hard_Enemy, self).__init__(*groups)  # initial position
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(pygame.Color('red'))
+        self.rect = self.image.get_rect()
+        self.x, self.y = Vector2(pos)
+        self.speed = 5
+        self.move_up = False
+        self.move_left = False
+        self.move_right = False
+
+    def move(self, player):
+        # find normalized direction vector (dx, dy) between enemy and player
+        dx, dy = self.rect.x - player.rect.x, self.rect.y - player.rect.y
+        dist = math.hypot(dx, dy)
+        dx, dy = dx / dist, dy / dist
+        # move along this normalized vector towards the player at current speed
+        self.x -= dx * self.speed
+        self.y -= dy * self.speed
+
+        print(self.x, self.y)
 
 
 class Hero(pygame.sprite.Sprite):
@@ -42,26 +67,29 @@ class Hero(pygame.sprite.Sprite):
         self.rect.center = self.pos
 
 
+SIZE = pygame.FULLSCREEN
+screen = pygame.display.set_mode((0, 0), SIZE)
+WIDTH = screen.get_width()
+HEIGHT = screen.get_height()
+
+
 def main():
     pygame.init()
-    SIZE = pygame.FULLSCREEN
-    screen = pygame.display.set_mode((0, 0), SIZE)
-    WIDTH = screen.get_width()
-    HEIGHT = screen.get_height()
-
     clock = pygame.time.Clock()
     camera = Vector2(WIDTH // 2, HEIGHT // 2)
-    hero = Hero((WIDTH // 2, HEIGHT // 2))
+    all_sprites = pygame.sprite.Group()
+    hero = Hero((WIDTH // 2, HEIGHT // 2), all_sprites)
+    enemy = Hard_Enemy((0, 0), all_sprites)
     # green rects
-    background_rects = [pygame.Rect(randrange(-3000, 3001), randrange(-3000, 3001), 30, 10) for _ in range(500)]
-
+    background_rects = [pygame.Rect(randrange(-2000, 2001), randrange(-3000, 3001), 30, 10) for _ in range(200)]
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
             hero.handle_event(event)
-        hero.update()
+        all_sprites.update()
+
         # A vector that points from the camera to the player.
         heading = hero.pos - camera
         # Follow the player with the camera.
@@ -71,15 +99,17 @@ def main():
         offset = -camera + Vector2(WIDTH // 2, HEIGHT // 2)  # centering player
         screen.fill((30, 30, 30))
         # Blit all objects and add the offset to their positions.
+        enemy.move(hero)
         for background_rect in background_rects:
             if hero.rect.colliderect(background_rect):
                 hero.dollars += 1
                 background_rects.remove(background_rect)
+                background_rects.append(pygame.Rect(randrange(-2000, 2001), randrange(-3000, 3001), 30, 10))
             else:
                 topleft = background_rect.topleft + offset
                 pygame.draw.rect(screen, pygame.Color('green'), (topleft, background_rect.size))
-
         screen.blit(hero.image, hero.rect.topleft + offset)
+        screen.blit(enemy.image, (enemy.x, enemy.y))
         pygame.display.flip()
         clock.tick(60)
 
